cb875851ba5c7aa6507cc7ee9ed1a9c404cf812b
Mandy-code-creator <i109143111@nkust.edu.tw>
2026-05-13 08:52:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.subplots as plt
+import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import re
